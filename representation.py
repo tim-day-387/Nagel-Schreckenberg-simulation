11f52a08bdab531d52701d4b28589d1fa560ffbf
Tim Day <tday141@gmail.com>
2021-10-03 22:11:37 -0400

    Cleaned up representation

--- representation.py
+++ representation.py
@@ -1,7 +1,10 @@
+# General Imports
 import pygame
 from infoDisplayer import *
 
+# Representation class
 class Representation():
+    # Constructor
     def __init__(self, screen, road, simulationManager):
         self.screen = screen
         self.width, self.heigth = screen.get_width(), screen.get_height()
@@ -15,18 +18,21 @@ class Representation():
 
         self.colors = [ (255, 0, 0), (180, 20, 0), (80, 60, 0), (100, 80, 0), (0, 180, 0), (0, 255, 0), (80, 120, 0), (60, 140, 0), (40, 160, 0) ]
 
+    # Draw objects on screen
     def draw(self, dt):
         self.__updateAcc(dt)
         self.screen.fill( (0, 0, 0) )
         self.drawRoad(dt)
         self.infoDisplayer.draw()
 
+    # Draw the general outline of the road
     def drawRoad(self, dt):
         y = 0
         for lane in self.road.lanes:
             self.__drawLane(lane, y, dt)
             y += self.cellSize
 
+    # Draw each lane in the road
     def __drawLane(self, lane, y, dt):
         x = 0
         for cell in lane:
@@ -38,24 +44,28 @@ class Representation():
 
             x += self.cellSize
 
+    # Draw each individual cell on the road
     def __drawCell(self, x, y, speedLimit):
         realPos = self.__getPosOnScreen((x,y))
         factor = 30 + speedLimit*30
         pygame.draw.rect(self.screen, (factor, factor, factor), (realPos[0], realPos[1], self.cellSize, self.cellSize), 0)
-#        pygame.draw.rect(self.screen, (0, 30, 200), (realPos[0], realPos[1], self.cellSize, self.cellSize), 2)
+        # pygame.draw.rect(self.screen, (0, 30, 200), (realPos[0], realPos[1], self.cellSize, self.cellSize), 2)
 
+    # Draw each car in its proper location
     def __drawCar(self, car, x, y):
         invProgress = (1 - self.acc / self.updateFrame)*self.cellSize
         offset = (car.prevPos[0] - car.pos[0])*invProgress, (car.prevPos[1] - car.pos[1])*invProgress
         realPos = self.__getPosOnScreen((x+offset[0], y+offset[1]))
         pygame.draw.rect(self.screen, self.colors[car.velocity], (realPos[0],realPos[1],self.cellSize,self.cellSize), 0)
 
+    # Update acc
     def __updateAcc(self, dt):
         self.acc += dt
         if self.acc >= self.updateFrame:
             self.infoDisplayer.update()
         self.acc = self.acc % (self.updateFrame + 0)
 
+    # Get position on screen, accounting for offsets
     def __getPosOnScreen(self, pos):
         x, y = pos
         while x + self.margins[0] >= self.width - self.margins[0]:
