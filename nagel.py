15931c2f0b2bb30fc8bf0e3e1dd3f10aaa237e5a
Tim Day <tday141@gmail.com>
2021-11-10 00:46:39 -0500

    Moved initialization in function, made more modular

--- nagel.py
+++ nagel.py
@@ -18,82 +18,116 @@ config = importlib.import_module(sys.argv[1])
 # Set Seed
 random.seed(config.seed)
 
-# Grab info from configs and create objects
-simulation.car.Car.slowDownProbability = config.slowDownProbability
-simulation.car.Car.laneChangeProbability = config.laneChangeProbability
-trafficGenerator = TestTrafficGenerator(0)
-speedLimits = [simulation.speedLimits.SpeedLimits(config.speedLimits, config.maxSpeed), \
-               simulation.speedLimits.SpeedLimits(config.speedLimits, config.maxSpeed), \
-               simulation.speedLimits.SpeedLimits(config.speedLimits, config.maxSpeed)]
-road = [simulation.road.Road(config.lanes, config.length, speedLimits[0]), \
-        simulation.road.Road(config.lanes, config.length, speedLimits[1]), \
-        simulation.road.Road(config.lanes, config.length, speedLimits[2])]
-simulation = [SimulationManager(road[0], trafficGenerator, config.updateFrame), \
-              SimulationManager(road[1], trafficGenerator, config.updateFrame), \
-              SimulationManager(road[2], trafficGenerator, config.updateFrame)]
-# intersections = [intersection(0, [1, 2])]
-intersections = [intersection(0, [1, 2]), intersection(1, [0]), intersection(2, [0])]
+# Create intersections list
+def createInters(numOfRoads, vertices):
+    output = []
 
-# Start simulation
-print("Simulation Started.")
+    for i in range(numOfRoads):
+        output.append(intersection(vertices[i][0], vertices[i][1]))
+
+    return output
+
+# Create speedLimts list
+def createSpeedLimits(numOfRoads):
+    output = []
+
+    for i in range(numOfRoads):
+        output.append(simulation.speedLimits.SpeedLimits(config.speedLimits, config.maxSpeed))
+
+    return output
+
+# Create roads list
+def createRoads(numOfRoads):
+    output = []
+
+    for i in range(numOfRoads):
+        output.append(simulation.road.Road(config.lanes, config.length, speedLimits[i]))
+
+    return output
+
+# Create simulations list
+def createSims(numOfRoads):
+    output = []
+
+    for i in range(numOfRoads):
+        output.append(SimulationManager(road[i], trafficGenerator, config.updateFrame))
+
+    return output
+
+# Main Simulation, Tables
+def main(numSims):
+    # Run each iteration 
+    for x in range(iterations):
+        # Run each simulation
+        for y in range(numSims):
+            # Decide whether to use initial value
+            if x == 0:
+                trafficGenerator.carPerUpdate = initialCars[y]
+            else:
+                trafficGenerator.carPerUpdate = allocated[y]
+
+            # Iterate simulation
+            simulation[y].makeStep()
+
+        # Reset allocated
+        allocated = [0] * (numSims + 1)
+
+        # Collect Stats on each simulation
+        for y in range(numSims):
+            # Update cars leaving roads
+            for i in intersections:
+                allocated = i.applyRule(allocated, simulation, road, deadCars)
+            
+            # Account for new deadCars
+            deadCars[y] = road[y].deadCars
+
+            # Collect stats
+            totalCars, avgSpeed = road[y].getAvgCarSpeed()
+            overallTotalCars[y] = totalCars + overallTotalCars[y]
+            overallAvgSpeed[y] = avgSpeed + overallAvgSpeed[y]
+        
+    # Table info
+    headers = ["Road", "Average Speed", "Average Number of Cars"]
+    data = np.zeros((numSims, 3))
+
+    # Populate table
+    for y in range(numSims):
+        data[y][0] = y
+        data[y][1] = overallAvgSpeed[y]/iterations
+        data[y][2] = overallTotalCars[y]/iterations
+
+    # Print final results
+    print(tabulate(data, headers))
 
 # Simulation parameters
-overallAvgSpeed = [0, 0, 0]
-sourceCars = 20
-initialCars = [200, 0, 0]
-# initialCars = [sourceCars, 0, 0]
-allocated = [0, 0, 0, 0]
-deadCars = [0, 0, 0]
-iterations = 500
-overallTotalCars = [0, 0, 0]
-numSims = len(simulation)
+numRoads = 8
+overallAvgSpeed = [0] * numRoads
+initialCars = [0] * numRoads
+allocated = [0] * (numRoads + 1)
+deadCars = [0] * numRoads
+iterations = 5000
+initialCars[0] = 20
+overallTotalCars = [0] * numRoads
+vertices = [[7, [1, 2, 0]], \
+            [1, [3, 4]], \
+            [2, [5, 6]], \
+            [3, [7]], \
+            [4, [7]], \
+            [5, [7]], \
+            [6, [7]], \
+            [0, [7]]]
 
-# Iterate all simulations 
-for x in range(iterations):
-    for y in range(numSims):
-        # Decide whether to use initial value
-        if x == 0:
-            trafficGenerator.carPerUpdate = initialCars[y]
-        else:
-            trafficGenerator.carPerUpdate = allocated[y]
 
-        # Iterate simulation
-        simulation[y].makeStep()
+# Set some simulation parameters
+simulation.car.Car.slowDownProbability = config.slowDownProbability
+simulation.car.Car.laneChangeProbability = config.laneChangeProbability
 
-    print("Allocated: ", x)
-    print(allocated)
+# Generate different objects
+trafficGenerator = TestTrafficGenerator(0)
+speedLimits = createSpeedLimits(numRoads)
+road = createRoads(numRoads)
+simulation = createSims(numRoads)
+intersections = createInters(numRoads, vertices)
 
-    # Reset allocated
-    allocated = [0, 0, 0, 0]
-        
-    for y in range(numSims):
-        # Update cars leaving roads
-        for i in intersections:
-            allocated = i.applyRule(allocated, simulation, road, deadCars)
-
-        # print(allocated)
-            
-        # Account for new deadCars
-        deadCars[y] = road[y].deadCars
-
-        # Collect stats
-        totalCars, avgSpeed = road[y].getAvgCarSpeed()
-        overallTotalCars[y] = totalCars + overallTotalCars[y]
-        overallAvgSpeed[y] = avgSpeed + overallAvgSpeed[y]
-
-    print("Dead Cars: ", x)
-    print(deadCars)
-        
-# Table info
-headers = ["Road", "Average Speed", "Average Number of Cars"]
-data = np.zeros((numSims, 3))
-
-# Populate table
-for y in range(numSims):
-    data[y][0] = y
-    data[y][1] = overallAvgSpeed[y]/iterations
-    data[y][2] = overallTotalCars[y]/iterations
-
-# Print final results
-print(tabulate(data, headers))
-print("Simulation Completed.")
+# Run the Program
+main(numRoads)
